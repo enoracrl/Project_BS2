ac5dca58c8c4799249338953a4017e0abd7c4a6e
Enora <Enora@dn037066.etu.univ-rennes1.fr>
2022-11-22 09:55:59 +0100

    adding some news demo + interaction with user

--- demo.py
+++ demo.py
@@ -20,93 +20,186 @@ file_cleaned = "toy_example_cleaned.txt"
 file_CC = "toy_example_with_3_CC.txt"
 
 if __name__ == "__main__" :
-    interactome1 = Interactome(file_test_1)   # objet de la classe Interactome
-    interactome2 = Interactome(file_test_2)
-    false_interactome_1 = Interactome(false_file_1)
-    false_interactome_2 = Interactome(false_file_2)
-    false_interactome_3 = Interactome(false_file_3)
-    interactome_to_clean = Interactome(file_to_clean)
-    interactome_CC = Interactome(file_CC)
-    #false_interactome_4 = Interactome(false_file_4)
-    print("List of interactions of toy_example : ", interactome1.get_int_list(), 
-          "List of interactions of Human_HighQuality (beginning) :", interactome2.get_int_list()[:4], 
-          sep="\n")
-    print('\n')
-    print("Dictionnary of interactions of toy_example : ", interactome1.get_int_dict(), 
-          "Dictionnary of interactions of Human_HighQuality (1433B_HUMAN) :", list(interactome2.get_int_dict().items())[0],
-          sep="\n")
-    print('\n')
-    print("Matrix of interactions of toy_example : ", interactome1.get_mat(), 
-          "Matrix of interactions of Human_HighQuality (beginning) :", interactome2.get_mat(), 
-          sep="\n")
-    print('\n')
-    print("Toy_example is a correct file : ", interactome1.is_interaction_file(), 
-          "Human_HighQuality is a correct file :", interactome2.is_interaction_file(), 
-          "A file without a number of interactions is a correct file :", false_interactome_1.is_interaction_file(), 
-          "An empty file is a correct file :", false_interactome_2.is_interaction_file(), 
-          "A file with a number of lines different than the number of interactions is a correct file :", false_interactome_3.is_interaction_file(), 
-          #"A file with a wrong number of columns is a correct file :", false_interactome_4.is_interaction_file(), 
-          sep="\n")
-    print('\n')
-    print("List of proteins and the matrix of interactions of toy_example : ", interactome1.read_interaction_file_mat(), 
-          "List of proteins and the matrix of interactions of Human_HighQuality :", (interactome2.read_interaction_file_mat()[0][:10],interactome2.read_interaction_file_mat()[1]), 
-          sep="\n")
-    print('\n')
-    print("Triplets of the dictionnary, list and the matrix of interactions of toy_example : ", interactome1.read_interaction_file(), 
-          #"Triplets of the dictionnary, list and the matrix of interactions of Human_HighQuality : ", interactome1.read_interaction_file()(0), 
-          sep="\n")
-    print('\n')
-    print("Number of vertices of toy_example : ", interactome1.count_vertices(), 
-          "Number of vertices of Human_HighQuality : ", interactome2.count_vertices(), 
-          sep="\n")
-    print('\n')
-    print("Number of edges of toy_example : ", interactome1.count_edges(), 
-          "Number of edges of Human_HighQuality : ", interactome2.count_edges(), 
-          sep="\n")
-    print('\n')
-    print("Interactions of a file which needs to be cleaned up :", interactome_to_clean.get_int_list(), sep="\n")
-    print("Interactions of the cleaned up file :")
-    interactome_to_clean.clean_interactome(file_cleaned)
-    interactome_cleaned = Interactome("toy_example_cleaned.txt")
-    print(interactome_cleaned.get_int_list())
-    print('\n')
-    print("Degrees of the protein A toy_example : ", interactome1.get_degree("A"), 
-          "Degrees of the protein 1433B_HUMAN Human_HighQuality : ", interactome2.get_degree("1433B_HUMAN"), 
-          sep="\n")
-    print('\n')
-    print("Maximum degree of toy_example : ", interactome1.get_max_degree(), 
-          "Maximum degree of Human_HighQuality : ", interactome2.get_max_degree(), 
-          sep="\n")
-    print('\n')
-    print("Average degree of toy_example : ", interactome1.get_ave_degree(), 
-          "Average degree of Human_HighQuality : ", interactome2.get_ave_degree(), 
-          sep="\n")
-    print('\n')
-    print("Number of proteins that have the same degree as 2 in toy_example : ", interactome1.count_degree(2), 
-          "Number of proteins that have the same degree as 20 in Human_HighQuality : ", interactome2.count_degree(20), 
-          sep="\n")
-    print('\n')
-    print("Number of proteins that have a degree between 2 and 4 in toy_example : ")
-    interactome1.histogram_degree(2, 4)
-    print("Number of proteins that have a degree between 10 and 20 in Human_HighQuality : ")
-    interactome2.histogram_degree(10,20)
-    print('\n')
-    print("Density of toy_example :", interactome1.density(), 
-          "Density of Human_HighQuality :", interactome2.density(), 
-          sep="\n" )
-    print('\n')
-    print("Local clustering coefficient of the protein A toy_example :", interactome1.clustering("A"), 
-          "Local clustering coefficient of the protein 1433B_HUMAN of Human_HighQuality :", interactome2.clustering("1433B_HUMAN"), 
-          sep="\n" )
-    print("test :", interactome_CC.find_CC())
-    print("test :", interactome_CC.count_CC())
-    print("test :", interactome_CC.write_CC())
-    print("test :", interactome_CC.extract_CC("A"))
-    print("test :", interactome_CC.extract_CC("D"))
-    print("test :", interactome_CC.extract_CC("H"))
-    print("test :", interactome_CC.compute_CC())
-    
-    
-    
+      interactome1 = Interactome(file_test_1)   # objet de la classe Interactome
+      interactome2 = Interactome(file_test_2)
+      false_interactome_1 = Interactome(false_file_1)
+      false_interactome_2 = Interactome(false_file_2)
+      false_interactome_3 = Interactome(false_file_3)
+      interactome_to_clean = Interactome(file_to_clean)
+      interactome_CC = Interactome(file_CC)
+      #false_interactome_4 = Interactome(false_file_4)
+      
+      print("\n","DEMO OF OUR PYTHON PROJECT ABOUT BIOLOGICAL NETWORKS", "\n")
+      input()
+      print("Presented by : Léa BEAULIEU and Enora CORLER", "\n")
+      input()
+      print("* CHAPTER 1 : READING A INTERACTIONS GRAPH BETWEEN PROTEINS *", "\n")
+      input()
+      print("Lists of interactions of :")
+      input()
+      print("toy_example : ", interactome1.get_int_list(), "\n",
+            "Human_HighQuality (beginning) :", interactome2.get_int_list()[:4], 
+            sep="\n", end="\n")
+      input()
+      
+      print("\n", "Dictionnary of interactions of :")
+      input()
+      print("toy_example : ", interactome1.get_int_dict(), "\n",
+            "Human_HighQuality (1433B_HUMAN) :", list(interactome2.get_int_dict().items())[0],
+            sep="\n", end="\n")
+      input()
 
-    
+      print("\n", "Matrix of interactions of :")
+      input()
+      print("toy_example : ", interactome1.get_mat(), "\n",
+            "Human_HighQuality (beginning) :", interactome2.get_mat(), 
+            sep="\n", end="\n")    
+      input()
+
+      print("\n", "Is this file correct ? :")
+      input()
+      print("toy_example : ", interactome1.is_interaction_file(), "\n",
+            "Human_HighQuality :", interactome2.is_interaction_file(), "\n",
+            "A file without a number of interactions :", false_interactome_1.is_interaction_file(), "\n",
+            "An empty file :", false_interactome_2.is_interaction_file(), "\n",
+            "A file with a number of lines different than the number of interactions is a correct file :", false_interactome_3.is_interaction_file(),
+            #"A file with a wrong number of columns is a correct file :", false_interactome_4.is_interaction_file(),
+            sep="\n", end="\n")
+      input()
+      
+      print("\n", "Lists of proteins and the matrix of interactions of :")
+      input()
+      print("toy_example : ", interactome1.read_interaction_file_mat(), "\n",
+            "Human_HighQuality :", (interactome2.read_interaction_file_mat()[0][:10],interactome2.read_interaction_file_mat()[1]), 
+            sep="\n", end="\n")
+      input()
+      
+      print("\n", "Triplets of the dictionnary, list and the matrix of interactions of ... :")
+      input()
+      print("toy_example : ", interactome1.read_interaction_file(), 
+            #"Triplets of the dictionnary, list and the matrix of interactions of Human_HighQuality : ", interactome1.read_interaction_file()(0), 
+            end='\n')
+      input()
+      
+      print("\n","* CHAPTER 2 : EXPLORATION OF AN INTERACTIONS GRAPH BETWEEN PROTEINS *", "\n")
+      input()
+      
+      print("Number of vertices of :")
+      input()
+      print("toy_example : ", interactome1.count_vertices(), "\n", 
+            "Human_HighQuality : ", interactome2.count_vertices(), 
+            end='\n')
+      input()
+      
+      print("Number of edges of ... :")
+      input()
+      print("toy_example : ", interactome1.count_edges(), "\n",
+            "Human_HighQuality : ", interactome2.count_edges(), 
+            end='\n')
+      input()
+      
+      print("Cleaning up file :")
+      input()
+      print("Interactions of a file which needs to be cleaned up :", interactome_to_clean.get_int_list())
+      print("Interactions of the cleaned up file :")
+      interactome_to_clean.clean_interactome(file_cleaned)
+      interactome_cleaned = Interactome("toy_example_cleaned.txt")
+      print(interactome_cleaned.get_int_list(), end="\n")
+      input()
+      
+      print("Degrees of the protein ... :")
+      input()
+      print("A in toy_example : ", interactome1.get_degree("A"), "\n",
+            "1433B_HUMAN in Human_HighQuality : ", interactome2.get_degree("1433B_HUMAN"), 
+            end="\n")
+      input()
+      
+      print("Maximum degree of ... :")
+      input()
+      print("toy_example : ", interactome1.get_max_degree(), "\n",
+            "Human_HighQuality : ", interactome2.get_max_degree(), 
+            end="\n")
+      input()
+      
+      print("Average degree of ... :")
+      input()
+      print("toy_example : ", interactome1.get_ave_degree(), "\n",
+            "Human_HighQuality : ", interactome2.get_ave_degree(), end="\n")
+      input()
+      
+      print("Number of proteins that have the same degree as ... :")
+      input()
+      print("2 in toy_example : ", interactome1.count_degree(2), "\n",
+            "20 in Human_HighQuality : ", interactome2.count_degree(20),
+            end="\n")
+      input()
+      
+      print("Representation of the number of proteins that have a degree between ... :")
+      input()
+      print("2 and 4 in toy_example : ", interactome1.histogram_degree(2, 4), "\n",
+            "10 and 20 in Human_HighQuality : ", interactome2.histogram_degree(10,20), 
+            end="\n")
+      input()
+      
+      print("* CHAPTER 3 : MODIFICATIONS OF SPECIFICATIONS, OOP *")
+      input()
+      
+      print("\n", "Density of ... :")
+      input()
+      print("toy_example :", interactome1.density(), "\n",
+            "Human_HighQuality :", interactome2.density(), 
+            end="\n" )
+      input()
+      
+      print("\n", "Local clustering coefficient of the protein ... :")
+      input()
+      print("A in toy_example :", interactome1.clustering("A"), "\n",
+            "1433B_HUMAN in Human_HighQuality :", interactome2.clustering("1433B_HUMAN"), 
+            end="\n" )
+      input()
+      
+      print("\n","* CHAPTER 4 : RANDOM GRAPHS *", "\n")
+      input()
+      print("Erdös-Rényi random graph with a probability p of 0.5")
+      input()
+      print(interactome1.graph_ER(0.5))
+      input()
+      print("Barabasi-Albert random graph with a maximal number of nodes of 10")
+      input()
+      print(interactome1.graph_BA(15))
+      input()
+      
+      print("\n","* CHAPTER 5 : CALCULATION OF THE CONNECTED COMPONENTS OF A PROTEIN INTERACTION GRAPH *", "\n")
+      input()
+      
+      print("interactome_CC is a interactome based on toy_example with", len(interactome_CC.get_int_dict()), "nodes", sep=' ', end="\n")
+      input()
+      
+      print("How many connected components are in this graph ?")
+      input()
+      print(interactome_CC.count_CC(), " nodes", end="\n")
+      input()
+      
+      print("Writing a file which contains the different connected components of a given graph :")
+      input()
+      interactome_CC.write_CC()
+      with open("connected_components.txt", "r", encoding="utf-8") as file_reader:
+            for line in file_reader :
+                  print(line)
+      input()
+      
+      print("\n", "All the vertices of the same connected component as ... :")
+      input()
+      print("protein A :", interactome_CC.extract_CC("A"), "\n", 
+            "protein D :", interactome_CC.extract_CC("D"), "\n", 
+            "protein H :", interactome_CC.extract_CC("H"), 
+            end="\n")
+      input()
+      print("Number of connected components for interactome_CC :", interactome_CC.compute_CC())
+      
+      input()
+      
+      
+      
+
+      
